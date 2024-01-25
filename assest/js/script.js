bb037672d04b54d6c12974eccf12bb855d0c4b67
David Costa <david.antunes.costa@hotmail.com>
2024-01-25 11:24:04 -0300

    Ajustando form, acrescentando wpp fixed and algumas responsividades

--- assest/js/script.js
+++ assest/js/script.js
@@ -1,5 +1,7 @@
 import initModal from './modules/modal.js';
 import initMenu from './modules/menu.js';
+import initScrollSuave from './modules/scroll-suave.js';
 
 initModal();
 initMenu();
+initScrollSuave();
